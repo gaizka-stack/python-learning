70236743599c5a5ccc017a117b1579b75763cc76
gaizka-stack <gaizkapradas@gmail.com>
2026-05-08 00:39:09 +0100

    asic f-string example

---
+++ basics/f_string_example.py
@@ -0,0 +1,15 @@
+"""
+Basic example of using f-strings in Python.
+"""
+
+def main():
+    integer_number = 7
+    float_number = 10.5
+
+    # Print values using f-strings
+    print(f"Integer number: {integer_number}")
+    print(f"Float number: {float_number}")
+
+
+if __name__ == "__main__":
+    main()
